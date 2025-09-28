5bcc66430b4400ae50892896775a20cb2bf0503c
MG2003 <max.guo2014@gmail.com>
2025-09-28 02:19:25 -0400

    bfs

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,31 @@
 extends Node3D
 
-var nodes = {
-	$UnitCube : [$UnitCube2]
-	$UnitCube2 
-}
+@export var Start : Tile
+
+@export var End : Tile
+
+
+func _ready():
+	print("hi")
+	bfs([[Start.paths[0]]])
+
+
+func bfs(q):
+	var seen = []
+	while len(q) > 0:
+		var next = q.pop_front()
+		var new = next[-1]
+		seen.append(new)
+		if new == End:
+			print(next)
+			break
+		for q_append in new.paths:
+			if seen.has(q_append) == false:
+				q.append(next + [q_append])
+			
+				
+		
+		
+		
+	
+	
---
+++ scripts/resources/tile.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+class_name Tile
+
+@export var paths : Array[Node3D]
