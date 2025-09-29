12e5400ac91cf70daf79889fcae0a0644006cac0
MG2003 <max.guo2014@gmail.com>
2025-09-29 01:04:08 -0400

    animations on pathfinding(incomplete)

--- scripts/level.gd
+++ scripts/level.gd
@@ -8,6 +8,10 @@ extends Node3D
 
 var Current : Tile
 
+@onready var path_queue = []
+
+var target_tile
+
 func _ready():
 	Current = Start
 	connect_signals()
@@ -23,12 +27,20 @@ func connect_signals():
 func _on_cube_clicked(cube: Tile) -> void:
 	print("kewb clicked")
 	var path = bfs(Current, cube)
-	for p in path:	
-		var pos = p.get_pos()
-		var offset = pos - Current.get_pos()
-		Player.translate(offset)
-		await get_tree().create_timer(1).timeout
-		Current = p
+	if path_queue == []:
+		target_tile = path.pop_front()		
+	path_queue = path
+		
+
+func _physics_process(delta :float):
+	if path_queue.is_empty():
+		return
+	Player.global_position = Player.global_position.move_toward(target_tile.get_pos() + Vector3(0, 1, 0), delta)
+	
+	if Player.global_position == target_tile.get_pos()+ Vector3(0, 1, 0):
+		Current = target_tile
+		target_tile = path_queue.pop_front()
+	
 	
 	
 
@@ -43,6 +55,7 @@ func bfs(start, seek):
 		seen.append(new)
 		if new == seek:
 			print(next)
+			next.pop_front()
 			return next
 		for q_append in new.paths:
 			if seen.has(q_append) == false:
