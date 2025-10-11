986d593d99758a449199f563bab773201b281b5e
MG2003 <max.guo2014@gmail.com>
2025-10-11 15:46:50 -0400

    heehee

--- scripts/resources/tile.gd
+++ scripts/resources/tile.gd
@@ -1,3 +1,4 @@
+
 extends Node3D
 
 class_name Tile
@@ -6,6 +7,11 @@ class_name Tile
 
 @export var is_foreground : bool = false
 
+@onready var mesh_instance: MeshInstance3D = $StaticBody3D/MeshInstance3D
+@export_flags_3d_render var cull_mask:= 1:
+	set(value):
+		cull_mask = value
+		print(cull_mask)
 # The tile that we go to if we press Spacebar (if any)
 @export var gravity_path : Tile
 
@@ -15,8 +21,8 @@ var original_mat : Material
 
 signal cube_clicked(this_cube)
 
+
 func my_static_body3d_clicked():
-	print("Tile: my_static_body3d_clicked")
 	emit_signal("cube_clicked", self)
 
 func get_my_active_edge_pos() -> Vector3:
@@ -28,23 +34,8 @@ func get_my_active_edge_pos() -> Vector3:
 		return point.global_position
 	
 func _ready():
-	var mesh = $StaticBody3D/MeshInstance3D
-	original_mat = mesh.get_active_material(0)
-	set_visiblity_layers()
-
-func set_visiblity_layers():
-	var mesh = $StaticBody3D/MeshInstance3D
-	original_mat = mesh.get_active_material(0)
-	if (is_foreground):
-	# 	If this tile is in the FOREGROUND (infront of the player)
-	#	Set the CULL LAYER TO 3
-		mesh.set_layer_mask_value(3,true)
-		mesh.set_layer_mask_value(1,false)
-	else:
-	#	If this tile is in the BACKGROUND (behind the player)
-	#	SET THE CULL LAYER TO 1
-		mesh.set_layer_mask_value(1,true)
-		mesh.set_layer_mask_value(3,false)	
+	original_mat = mesh_instance.get_active_material(0)
+	mesh_instance.set_layer_mask(cull_mask)
 
 func turn_red():
 	var mesh = $StaticBody3D/MeshInstance3D
@@ -71,3 +62,4 @@ func reset_material():
 	var mesh = $StaticBody3D/MeshInstance3D
 	if original_mat:
 		mesh.set_surface_override_material(0, original_mat)
+		
