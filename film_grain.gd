736338b18c4632d8badabd92a251f3d5b2f7566e
bcherek <ben_cherek@brown.edu>
2025-10-18 14:46:13 -0400

    background, film texture

---
+++ film_grain.gd
@@ -0,0 +1,15 @@
+extends TextureRect
+
+@onready var rng = RandomNumberGenerator.new()
+
+
+@export var seconds_between_film_swaps = 0
+
+var seconds_since_last_swap = 0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	texture.noise.seed = rng.randi()
+	#seconds_since_last_swap += delta
+	#if seconds_between_film_swaps < seconds_since_last_swap:
+		#texture.noise.seed = rng.randi()
+		#seconds_since_last_swap = 0
